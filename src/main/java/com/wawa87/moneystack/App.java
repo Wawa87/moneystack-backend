bd367fc7bff865e97ea2db2b28f6a2be24fbe396
wawa87 <anthony@carbonitemail.com>
2026-02-26 21:22:36 -0800

    Add embedded jetty, starter filter, and starter servlet.

---
+++ src/main/java/com/wawa87/moneystack/App.java
@@ -0,0 +1,35 @@
+package com.wawa87.moneystack;
+
+import com.wawa87.moneystack.service.auth.AuthenticationServlet;
+import com.wawa87.moneystack.service.auth.DispatcherFilter;
+import jakarta.servlet.DispatcherType;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.EnumSet;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler context =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        FilterHolder dispatcherFilter = new FilterHolder(new DispatcherFilter());
+        context.addFilter(
+                dispatcherFilter,
+                "/*",
+                EnumSet.of(DispatcherType.REQUEST)
+        );
+
+        ServletHolder authenticationServlet = new ServletHolder(new AuthenticationServlet());
+        context.addServlet(authenticationServlet, "/authentication");
+
+        server.start();
+        server.join();
+    }
+}
